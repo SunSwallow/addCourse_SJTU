514d1a57dca9b2cf671b6a313af976589439eace
windcoat <958493843@q.com>
2021-09-14 00:55:36 +0800

    change flash frequency

--- addcourse.js
+++ addcourse.js
@@ -1,8 +1,8 @@
 //配置参数
 PREFERENCE_MODE = false
 
-TEACHER_NAME = '邓大萌' //目标老师名字
-TIME_AND_PLACE = '星期一'//上课时间地点,设置为空字符串则匹配所有时间
+TEACHER_NAME = 'Mounir Hajli' //目标老师名字
+TIME_AND_PLACE = '星期四'//上课时间地点,设置为空字符串则匹配所有时间
 
 //主体代码
 function sleep(sleepTime) {
@@ -15,9 +15,9 @@ function isFull(str){
 }
 
 function clickAdd(lesson_info){
-    console.log("选择"+lesson_info[2].textContent)
+    console.log("选择"+lesson_info[2].textContent);
     lesson_info[8].getElementsByTagName("a")[0].click();
-    sleep(50);
+    sleep(30);
     document.querySelector("button").click()
     clearInterval(pid);
 }
@@ -37,7 +37,7 @@ function addCourse(lessons_tr, only_teacher_mode, only_time_place_mode){
                             clickAdd(lesson_info);
                             return true;
                         }else{
-                            console.log(time_and_place + '非目标时间')
+                            console.log(teacher+'  '+time_and_place + '非目标时间')
                         }
                     }else{
                         clickAdd(lesson_info);
@@ -59,21 +59,21 @@ function addCourse(lessons_tr, only_teacher_mode, only_time_place_mode){
 
 function flash(){
     document.getElementsByClassName("zeromodal-btn zeromodal-btn-primary querybtn")[0].click(); // 点击查询
-    console.log(flash_times);
+    console.log('刷新次数：'+flash_times);
     flash_times=flash_times+1;
     let lessons_tr =document.getElementsByClassName("zero-grid zero-grid-hover")[0].getElementsByTagName("tr");
     console.log("第一遍扫描，只扫描目标老师与目标时间")
-    flag = addCourse(lessons_tr, only_teacher_mode = true, only_time_place_mode=true)
+    flag = addCourse(lessons_tr, only_teacher_mode = true, only_time_place_mode=true);
     if (flag){return 0;}
     if (!PREFERENCE_MODE){
         console.log("第二遍扫描,扫描目标老师所有时间")
-        flag = addCourse(lessons_tr, only_teacher_mode = true, only_time_place_mode=false)
+        flag = addCourse(lessons_tr, only_teacher_mode = true, only_time_place_mode=false);
         if (flag){return 0;}
         console.log("第三遍扫描,扫描所有老师所有时间")
-        flag = addCourse(lessons_tr, only_teacher_mode = false, only_time_place_mode=false)
+        flag = addCourse(lessons_tr, only_teacher_mode = false, only_time_place_mode=false);
         if (flag){return 0;}
     }
     console.log('\t\t');
 }
 var flash_times=0;
-pid = setInterval(flash, 300);
+pid = setInterval(flash, 60);
